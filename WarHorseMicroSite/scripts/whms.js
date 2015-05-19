8a9a2fef0f41e84b56397a52146697955b7284b9
Hein <heinschulie@gmail.com>
2015-05-19 08:17:17 +0200

    WarHorseMicro

---
+++ WarHorseMicroSite/scripts/whms.js
@@ -0,0 +1,18 @@
+﻿$(".imagegallery").slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    variableWidth: true,
+    centerMode: true,
+    //centerPadding: '160px',
+    slidesToShow: 1
+});
+
+
+
+    //jssor_slider1_starter = function (containerId) {
+    //    var options = { $AutoPlay: true };
+    //    var jssor_slider1 = new $JssorSlider$(containerId, options);
+    //};
+
+    //jssor_slider1_starter('slider1_container');
